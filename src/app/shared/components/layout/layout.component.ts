a60b50a9494b03e4ff7760653e0115d4748e4c59
Ivan Manrique <imanrique@seek.pe>
2026-03-15 13:16:01 -0400

    feat: created layout component

---
+++ src/app/shared/components/layout/layout.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { delay, Subscription } from 'rxjs';
+import { LoaderService } from '../../services/loader/loader.service';
+import { Route, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss']})
+
+export class LayoutComponent implements OnInit, OnDestroy {
+  public subscription = new Subscription
+  public loader: any = {}
+  loaderSubscription: Subscription = new Subscription;
+  isHomePage = false;
+
+
+  @ViewChild('drawer') drawer!: any;
+  @ViewChild('filterDrawer') filterDrawer!: any;
+  
+  
+  showMenu = true;
+  typeFilter: any;
+  indexTabGeneral: any;
+
+
+  constructor(private loaderService: LoaderService, private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.showLoader();
+        setTimeout(() => {
+            this.loader = false;
+        }, 1000);
+    
+  }
+
+  showLoader() {
+        this.loaderSubscription = this.loaderService.getLoaderState().pipe(delay(0)).subscribe(
+            (response: any) => {
+                this.loader = response
+            }
+        )
+    }
+
+  ngOnDestroy(): void {
+    this.loader = false;
+    this.subscription.unsubscribe();
+    this.loaderSubscription.unsubscribe();
+  }
+
+}
+
